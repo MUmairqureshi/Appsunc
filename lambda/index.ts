2f6ffd542a4f43f2189009d182578281e00826b0
MUmairqureshi <67610498+MUmairqureshi@users.noreply.github.com>
2024-05-25 13:32:14 +0500

    update

---
+++ lambda/index.ts
@@ -0,0 +1,48 @@
+
+// import getTodos from './getTodos';
+// import updateTodo from "./updateTodo";
+// import addTodo from "./addTodo";
+// export type Todo = {
+//     id: string;
+//     title: string;
+//     done: boolean;
+// }
+
+// export type User = {
+//     id: string;
+//     title: string;
+//     done: boolean;
+// }
+
+// import { AppSyncResolverEvent } from 'aws-lambda';
+
+// export async function handler(event: AppSyncResolverEvent<any>)  {
+//     console.log("event ", event);
+//     switch (event.info.fieldName) {
+//         case "addTodo":
+//             return addTodo(event.arguments.todo);
+//         case "getTodos":
+//             return getTodos();
+//         case "updateTodo":
+//             return updateTodo(event.arguments);
+//         default:
+//             return null;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exports.handler =async () =>{
+    return "Hello World"
+}
--- lib/appsync-stack.ts
+++ lib/appsync-stack.ts
@@ -1,16 +1,59 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as appsync from '@aws-cdk/aws-appsync-alpha'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AppsyncStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const api = new appsync.GraphqlApi(this, "GRAPHQLTEST_API", {
+      name: 'test-api',
+      schema: appsync.SchemaFile.fromAsset('schema/schema.gql'),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY
+        }
+      }
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AppsyncQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    })
+    new cdk.CfnOutput(this , "APIGraphURL" ,{
+      value : api.graphqlUrl
+    })
+
+    new cdk.CfnOutput(this , "APIGraphAPIKey" ,{
+      value : api.apiKey ||""
+    })
+    
+
+
+    const lambdaFnAppsync = new cdk.aws_lambda.Function(this, "appsynctestLambda", {
+      functionName: `appsynctestlambda`,
+      runtime: cdk.aws_lambda.Runtime.NODEJS_14_X,
+      code: cdk.aws_lambda.Code.fromAsset("lambda"),
+      handler: "index.handler",
+    })
+    
+    // const lambda_function = new cdk.aws_lambda.Function(this, "LambdaFucntion", {
+    //   runtime: cdk.aws_lambda.Runtime.NODEJS_12_X,            ///set nodejs runtime environment
+    //   code: cdk.aws_lambda.Code.fromAsset("lambda"),          ///path for lambda function directory
+    //   handler: 'index.handler',                       ///specfic fucntion in specific file
+    //   timeout: cdk.Duration.seconds(10)               ///Time for function to break. limit upto 15 mins
+    // })
+    
+    // set lembda dataSource
+    // const lambda_data_source = api.addLambdaDataSource('lambdaDatSource' , lambdaFnAppsync)
+    const lambda_data_source = api.addLambdaDataSource("lamdaDataSource", lambdaFnAppsync);
+       ///Describing resolver for datasource
+       lambda_data_source.createResolver({
+        typeName: "Query",
+        fieldName: "hello"
+      })
+      // lambda_data_source.createResolver({
+      //   typeName: "Query",
+      //   fieldName: "hello"
+      // })
   }
 }
